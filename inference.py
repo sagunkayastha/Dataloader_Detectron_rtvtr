f46e7fa5c656c3318948700003da6b31d4116f4e
sagunkayastha <sgn.kayastha@gmail.com>
2020-01-28 16:22:01 +0545

    inference

--- inference.py
+++ inference.py
@@ -47,13 +47,8 @@ print(type(predictor))
 from detectron2.utils.visualizer import ColorMode
 
 for d in random.sample(dict_, 3):    
-    im = cv2.imread(d["file_name"])
-    outputs = predictor(im)
-    v = Visualizer(im[:, :, ::-1],
-                   metadata=rtvtr_metadata, 
-                   scale=0.8, 
-                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels
-    )
-    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    img = cv2.imread(d["file_name"])
+    visualizer = Visualizer(img[:, :, ::-1], metadata=fruits_nuts_metadata, scale=0.5)
+    vis = visualizer.draw_dataset_dict(d)
     #cv2_imshow(v.get_image()[:, :, ::-1])
 	cv2.imwrite('AI/test_'+d+'.jpg',v.get_image()[:, :, ::-1])
